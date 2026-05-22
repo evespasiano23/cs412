ae41bdde4df7eb137cfb7e5b3771bfd772e0c5e8
Emily Vespasiano <evespa@bu.edu>
2026-05-21 23:56:45 -0400

    Got show_all_profiles webpage working. Edited base.html, showall_profiles.html.

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,9 +1,10 @@
 # mini_insta/urls.py
 # url patterns for the 'mini_insta' app
 from django.urls import path
-from . import views
+from .views import ProfileListView
 
 
 urlpatterns = [
- 
+    path('', ProfileListView.as_view(), name='show_all_profiles'), 
 ]
+
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,14 @@
+# mini_insta/views.py
+# views for the mini_insta application
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Profile
+import random
 
 # Create your views here.
+class ProfileListView(ListView):
+    '''Define a view class to show all mini_insta Profiles.'''
+
+    model = Profile
+    template_name = "mini_insta/show_all_profiles.html"
+    context_object_name = "profiles" # want similar to model name. will contain many instances of profile
